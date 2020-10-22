17a81c4a292a431350eb0b179135572a60a5773b
Michael C. Main <deusprogrammer@gmail.com>
2020-10-22 16:48:02 -0500

    Updated forms a bit

--- src/forms/AbilityForm.jsx
+++ src/forms/AbilityForm.jsx
@@ -95,58 +95,74 @@ export default (props) => {
                                 </Select>
                             </td>
                         </tr>
+                        <tr>
+                            <td style={{fontWeight: "bolder"}}>
+                                Buffs:
+                            </td>
+                            <td>
+                                <Text field="buffs"></Text>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{fontWeight: "bolder"}}>
+                                Buffs Duration (in ticks):
+                            </td>
+                            <td>
+                                <Text field="buffsDuration" type="number"></Text>
+                            </td>
+                        </tr>
                     </fieldset>
                     <fieldset>
-                            <legend>Modifiers</legend>
-                            <tr>
-                                <td style={{fontWeight: "bolder"}}>
-                                    HP Modifier:
-                                </td>
-                                <td>
-                                    <Text field="mods.hp" type="number"></Text>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{fontWeight: "bolder"}}>
-                                    Hit Modifier:
-                                </td>
-                                <td>
-                                    <Text field="mods.hit" type="number"></Text>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{fontWeight: "bolder"}}>
-                                    Strength Modifier:
-                                </td>
-                                <td>
-                                    <Text field="mods.str" type="number"></Text>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{fontWeight: "bolder"}}>
-                                    Dexterity Modifier:
-                                </td>
-                                <td>
-                                    <Text field="mods.dex" type="number"></Text>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{fontWeight: "bolder"}}>
-                                    Intelligence Modifier:
-                                </td>
-                                <td>
-                                    <Text field="mods.int" type="number"></Text>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{fontWeight: "bolder"}}>
-                                    Armor Class Modifier:
-                                </td>
-                                <td>
-                                    <Text field="mods.ac" type="number"></Text>
-                                </td>
-                            </tr>
-                        </fieldset>
+                        <legend>Modifiers</legend>
+                        <tr>
+                            <td style={{fontWeight: "bolder"}}>
+                                HP Modifier:
+                            </td>
+                            <td>
+                                <Text field="mods.hp" type="number"></Text>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{fontWeight: "bolder"}}>
+                                Hit Modifier:
+                            </td>
+                            <td>
+                                <Text field="mods.hit" type="number"></Text>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{fontWeight: "bolder"}}>
+                                Strength Modifier:
+                            </td>
+                            <td>
+                                <Text field="mods.str" type="number"></Text>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{fontWeight: "bolder"}}>
+                                Dexterity Modifier:
+                            </td>
+                            <td>
+                                <Text field="mods.dex" type="number"></Text>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{fontWeight: "bolder"}}>
+                                Intelligence Modifier:
+                            </td>
+                            <td>
+                                <Text field="mods.int" type="number"></Text>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{fontWeight: "bolder"}}>
+                                Armor Class Modifier:
+                            </td>
+                            <td>
+                                <Text field="mods.ac" type="number"></Text>
+                            </td>
+                        </tr>
+                    </fieldset>
                 </tbody>
             </table>
             <button type="submit">Submit</button>
