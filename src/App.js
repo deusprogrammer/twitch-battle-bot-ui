8a91539af2bdfd80a27eca3b7d238dffc83df7bb
Michael C. Main <deusprogrammer@gmail.com>
2020-10-28 13:38:53 -0500

    Adding something to hide admin consoles

--- src/App.js
+++ src/App.js
@@ -11,32 +11,52 @@ import Monsters from './components/Monsters';
 import Ability from './components/Ability';
 import Abilities from './components/Abilities';
 
+import ApiHelper from './utils/ApiHelper';
+
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+    state = {
+        isAdmin: false
+    }
+
+    componentDidMount = async () => {
+        let user = await ApiHelper.getUser(username);
+        let profile = await axios.get(`https://deusprogrammer.com/api/profile-svc/users/${user.name}`, {
+            headers: {
+                "X-Access-Token": localStorage.getItem("accessToken")
+            }
+        });
+
+        if (profile.roles.includes("SUPER_USER")) {
+            this.setState({isAdmin: true});
+        }
+    }
     
-
-    return (
-        <div style={{width: "80%", margin: "auto"}}>
-            <ToastContainer />
-            <Router>
-                <div style={{textAlign: "center"}}>
-                    <Link to={`${process.env.PUBLIC_URL}/`}>Guide</Link> | <Link to={`${process.env.PUBLIC_URL}/battlers/~self`}>Battler</Link> | <Link to={`${process.env.PUBLIC_URL}/items`}>Items</Link> | <Link to={`${process.env.PUBLIC_URL}/abilities`}>Abilities</Link> | <Link to={`${process.env.PUBLIC_URL}/monsters`}>Monsters</Link>
-                </div>
-                <Switch>
-                    <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
-                    <Route exact path={`${process.env.PUBLIC_URL}/items`} component={Items} />
-                    <Route exact path={`${process.env.PUBLIC_URL}/items/:id`} component={Item} />
-                    <Route exact path={`${process.env.PUBLIC_URL}/abilities`} component={Abilities} />
-                    <Route exact path={`${process.env.PUBLIC_URL}/abilities/:id`} component={Ability} />
-                    <Route exact path={`${process.env.PUBLIC_URL}/monsters`} component={Monsters} />
-                    <Route exact path={`${process.env.PUBLIC_URL}/monsters/:id`} component={Monster} />
-                    <Route exact path={`${process.env.PUBLIC_URL}/battlers/:id`} component={Battler} />
-                </Switch>
-            </Router>
-        </div>
-  );
+    render() {
+        return (
+            <div style={{width: "80%", margin: "auto"}}>
+                <ToastContainer />
+                <Router>
+                    <div style={{textAlign: "center"}}>
+                        <Link to={`${process.env.PUBLIC_URL}/`}>Guide</Link> | <Link to={`${process.env.PUBLIC_URL}/battlers/~self`}>Battler</Link> | {this.state.isAdmin ? <Link to={`${process.env.PUBLIC_URL}/items`}>Items</Link> | <Link to={`${process.env.PUBLIC_URL}/abilities`}>Abilities</Link> | <Link to={`${process.env.PUBLIC_URL}/monsters`}>Monsters</Link> : null}
+                    </div>
+                    <Switch>
+                        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+                        <Route exact path={`${process.env.PUBLIC_URL}/items`} component={Items} />
+                        <Route exact path={`${process.env.PUBLIC_URL}/items/:id`} component={Item} />
+                        <Route exact path={`${process.env.PUBLIC_URL}/abilities`} component={Abilities} />
+                        <Route exact path={`${process.env.PUBLIC_URL}/abilities/:id`} component={Ability} />
+                        <Route exact path={`${process.env.PUBLIC_URL}/monsters`} component={Monsters} />
+                        <Route exact path={`${process.env.PUBLIC_URL}/monsters/:id`} component={Monster} />
+                        <Route exact path={`${process.env.PUBLIC_URL}/battlers/:id`} component={Battler} />
+                    </Switch>
+                </Router>
+            </div>
+    );
+    }
 }
 
 export default App;
