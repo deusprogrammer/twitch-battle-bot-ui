87ea3e6912508541c1abc5c69ed79aa1defe626c
Michael C. Main <deusprogrammer@gmail.com>
2020-11-05 17:55:38 -0600

    Cleaning up UI

--- src/elements/ItemElement.jsx
+++ src/elements/ItemElement.jsx
@@ -9,33 +9,35 @@ export default (props) => {
     }
     return (
         <div className="item">
-            <div className="item-image"><img src="https://dummyimage.com/100X100/000/fff" /></div>
-            <div className="item-details">
-                <div className="item-header"><span className="item-name">{item.name}</span><span className="item-type">{item.slot !== "hand" ? item.slot : null} {item.type}</span></div>
-                <div className="item-description">{item.description}</div>
-                <div className="item-stats">
-                    <div>{value} <strong>{(item.ac ? "armor " : null) || (item.dmg ? ` damage to ${item.dmgStat}` : null)} </strong></div>
-                    {["STR", "DEX", "INT"].map((modStat) => {
-                        return <div style={{float: "left"}} className={item.toHitStat === modStat ? "item-stat-highlight" : "item-stat"}>{modStat}: {item.mods[modStat.toLowerCase()]}</div>
-                    })}
+            <div className="item-inner">
+                <div className="item-image"><img src="https://dummyimage.com/100X100/000/fff" /></div>
+                <div className="item-details">
+                    <div className="item-header"><span className="item-name">{item.name}</span><span className="item-type">{item.slot !== "hand" ? item.slot : null} {item.type}</span></div>
+                    <div className="item-description">{item.description}</div>
+                    <div className="item-stats">
+                        <div>{value} <strong>{(item.ac ? "armor " : null) || (item.dmg ? ` damage to ${item.dmgStat}` : null)} </strong></div>
+                        {["STR", "DEX", "INT"].map((modStat) => {
+                            return <div style={{float: "left"}} className={item.toHitStat === modStat ? "item-stat-highlight" : "item-stat"}>{modStat}: {item.mods[modStat.toLowerCase()]}</div>
+                        })}
+                        <div style={{clear: "both"}} />
+                        {["HIT", "AC", "AP"].map((modStat) => {
+                            return <div style={{float: "left"}} className={item.toHitStat === modStat ? "item-stat-highlight" : "item-stat"}>{modStat}: {item.mods[modStat.toLowerCase()]}</div>
+                        })}
+                    </div>
                     <div style={{clear: "both"}} />
-                    {["HIT", "AC", "AP"].map((modStat) => {
-                        return <div style={{float: "left"}} className={item.toHitStat === modStat ? "item-stat-highlight" : "item-stat"}>{modStat}: {item.mods[modStat.toLowerCase()]}</div>
-                    })}
+                    <div className="item-triggers">
+                        {item.triggers.map((trigger) => {
+                            return <div style={{float: "left"}}>{trigger.abilityId} ({trigger.chance}% chance)</div>
+                        })}
+                    </div>
+                    <div style={{clear: "both"}} />
+                    <div className="item-abilities">
+                        {item.abilities.map((ability) => {
+                            return <div style={{float: "left"}}>{ability.abilityId}</div>
+                        })}
+                    </div>
+                    <div className="item-value"><span>{item.value} gold</span><span>{rarity.map(star => star)}</span></div>
                 </div>
-                <div style={{clear: "both"}} />
-                <div className="item-triggers">
-                    {item.triggers.map((trigger) => {
-                        return <div style={{float: "left"}}>{trigger.abilityId} ({trigger.chance}% chance)</div>
-                    })}
-                </div>
-                <div style={{clear: "both"}} />
-                <div className="item-abilities">
-                    {item.abilities.map((ability) => {
-                        return <div style={{float: "left"}}>{ability.abilityId}</div>
-                    })}
-                </div>
-                <div className="item-value"><span>{item.value} gold</span><span>{rarity.map(star => star)}</span></div>
             </div>
         </div>
     );
