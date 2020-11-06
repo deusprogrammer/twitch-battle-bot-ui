e74c1aa7adedb5047ddc4c3b3a14f4a4033bac44
Michael C. Main <deusprogrammer@gmail.com>
2020-11-06 15:22:38 -0600

    Added unicode stars

--- src/components/Battler.jsx
+++ src/components/Battler.jsx
@@ -153,141 +153,139 @@ export default class Battler extends React.Component {
         return (
             <div>
                 <h1>{user.name}</h1>
-                <div style={{marginLeft: "50px"}}>
-                    <div style={{display: "table-row"}}>
-                        <div style={{display: "table-cell", paddingRight: "50px"}}>
-                            <h3>Stats</h3>
+                <div>
+                    <div style={{float: "left"}}>
+                        <h3>Stats</h3>
+                        <table style={{marginLeft: "10px"}}>
+                            <tbody>
+                                <tr title="Your health points determine whether you are alive or not.  Once you hit zero, it's over." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>HP</td>
+                                    <td>{user.hp}/{user.maxHp}</td>
+                                </tr>
+                                <tr title="Your action points are consumed when you perform actions like attacking or using abilities." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>AP</td>
+                                    <td style={{textAlign: "center"}}>{user.ap}</td>
+                                </tr>
+                                <tr title="Your strength determines how hard you hit when you attack." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>STR</td>
+                                    <td style={{textAlign: "center"}}>{user.str}</td>
+                                </tr>
+                                <tr title="Your dexterity determines how often you can attack." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>DEX</td>
+                                    <td style={{textAlign: "center"}}>{user.dex}</td>
+                                </tr>
+                                <tr title="Your intelligence determines if your abilities succeed or not." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>INT</td>
+                                    <td style={{textAlign: "center"}}>{user.int}</td>
+                                </tr>
+                                <tr title="Your hit determines whether your attacks hit or not." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>HIT</td>
+                                    <td style={{textAlign: "center"}}>{user.hit}</td>
+                                </tr>
+                                <tr title="Your armor class determines how hard it is to hit you." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>AC</td>
+                                    <td style={{textAlign: "center"}}>{user.totalAC}</td>
+                                </tr>
+                                <tr title="Gold is used for purchasing things." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>Gold</td>
+                                    <td style={{textAlign: "center"}}>{user.gold}</td>
+                                </tr>
+                                <tr title="This is the range of damage you can do with your current weapon." style={{cursor: "pointer"}}>
+                                    <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>Damage Range</td>
+                                    <td style={{textAlign: "center"}}>{this.damageRange(user).low} - {this.damageRange(user).high}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div style={{float: "left"}}>
+                        <h3>Equipment</h3>
+                        <table style={{marginLeft: "10px"}}>
+                                { Object.keys(user.equipment).map((slot) => {
+                                    let item = user.equipment[slot];
+                                    return (
+                                        <React.Fragment>
+                                            <tr 
+                                                data-tip 
+                                                data-for={`${item.slot}-tip`} 
+                                                style={{cursor: "pointer"}}>
+                                                    <td style={{textAlign: "center", background: "teal", color: "white", fontWeight: "bolder"}}>{slot.toUpperCase()}</td>
+                                                    <td>{item.name}</td>
+                                            </tr>
+                                            <ReactToolTip id={`${item.slot}-tip`} place="right" effect="solid" delayHide={500} delayShow={500} delayUpdate={500}>
+                                                <ItemElement 
+                                                    item={item} 
+                                                    abilityTable={this.abilityTable} />
+                                            </ReactToolTip>
+                                        </React.Fragment>
+                                    )
+                                })}
+                        </table>
+                    </div>
+                    <div style={{float: "left"}}>
+                        <h3>Abilities</h3>
+                        {user.abilities.length > 0 ?
                             <table style={{marginLeft: "10px"}}>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>AP Cost</th>
+                                        <th>Element</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
                                 <tbody>
-                                    <tr title="Your health points determine whether you are alive or not.  Once you hit zero, it's over." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>HP</td>
-                                        <td>{user.hp}/{user.maxHp}</td>
-                                    </tr>
-                                    <tr title="Your action points are consumed when you perform actions like attacking or using abilities." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>AP</td>
-                                        <td style={{textAlign: "center"}}>{user.ap}</td>
-                                    </tr>
-                                    <tr title="Your strength determines how hard you hit when you attack." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>STR</td>
-                                        <td style={{textAlign: "center"}}>{user.str}</td>
-                                    </tr>
-                                    <tr title="Your dexterity determines how often you can attack." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>DEX</td>
-                                        <td style={{textAlign: "center"}}>{user.dex}</td>
-                                    </tr>
-                                    <tr title="Your intelligence determines if your abilities succeed or not." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>INT</td>
-                                        <td style={{textAlign: "center"}}>{user.int}</td>
-                                    </tr>
-                                    <tr title="Your hit determines whether your attacks hit or not." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>HIT</td>
-                                        <td style={{textAlign: "center"}}>{user.hit}</td>
-                                    </tr>
-                                    <tr title="Your armor class determines how hard it is to hit you." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>AC</td>
-                                        <td style={{textAlign: "center"}}>{user.totalAC}</td>
-                                    </tr>
-                                    <tr title="Gold is used for purchasing things." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>Gold</td>
-                                        <td style={{textAlign: "center"}}>{user.gold}</td>
-                                    </tr>
-                                    <tr title="This is the range of damage you can do with your current weapon." style={{cursor: "pointer"}}>
-                                        <td style={{background: "teal", color: "white", fontWeight: "bolder"}}>Damage Range</td>
-                                        <td style={{textAlign: "center"}}>{this.damageRange(user).low} - {this.damageRange(user).high}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div style={{display: "table-cell", paddingRight: "50px"}}>
-                            <h3>Equipment</h3>
-                            <table style={{marginLeft: "10px"}}>
-                                    { Object.keys(user.equipment).map((slot) => {
-                                        let item = user.equipment[slot];
-                                        return (
-                                            <React.Fragment>
-                                                <tr 
-                                                    data-tip 
-                                                    data-for={`${item.slot}-tip`} 
-                                                    style={{cursor: "pointer"}}>
-                                                        <td style={{textAlign: "center", background: "teal", color: "white", fontWeight: "bolder"}}>{slot.toUpperCase()}</td>
-                                                        <td>{item.name}</td>
-                                                </tr>
-                                                <ReactToolTip id={`${item.slot}-tip`} place="right" effect="solid" delayHide={500} delayShow={500} delayUpdate={500}>
-                                                    <ItemElement 
-                                                        item={item} 
-                                                        abilityTable={this.abilityTable} />
-                                                </ReactToolTip>
-                                            </React.Fragment>
-                                        )
-                                    })}
-                            </table>
-                        </div>
-                        <div style={{display: "table-cell", paddingRight: "50px"}}>
-                            <h3>Abilities</h3>
-                            {user.abilities.length > 0 ?
-                                <table style={{marginLeft: "10px"}}>
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>AP Cost</th>
-                                            <th>Element</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        { ["NONE", "FIRE", "ICE", "LIGHTNING", "WATER", "EARTH", "DARK", "LIGHT", "HEALING", "BUFFING", "CLEANSING"].map((element) => {
-                                            return user.abilities.map((ability) => {
-                                                if (element !== ability.element) {
-                                                    return;
-                                                }
+                                    { ["NONE", "FIRE", "ICE", "LIGHTNING", "WATER", "EARTH", "DARK", "LIGHT", "HEALING", "BUFFING", "CLEANSING"].map((element) => {
+                                        return user.abilities.map((ability) => {
+                                            if (element !== ability.element) {
+                                                return;
+                                            }
 
-                                                return (
-                                                    <React.Fragment>
-                                                        <tr data-tip
-                                                            data-for={`${ability.id}-tip`}
-                                                            style={{cursor: "pointer"}}
-                                                            key={`ability-${ability.id}`}>
-                                                                <td>{ability.name}</td>
-                                                                <td>{ability.ap}</td>
-                                                                <td>{ability.element}</td>
-                                                                <td>
-                                                                    <button onClick={() => {navigator.clipboard.writeText(ability.id);toast("Copied id to clipboard", {type: "info"});}}>Get Id</button>
-                                                                </td>
-                                                        </tr>
-                                                        <ReactToolTip id={`${ability.id}-tip`} place="right" effect="solid" delayHide={500} delayShow={500} delayUpdate={500}>
-                                                            <AbilityElement 
-                                                                ability={ability} 
-                                                                abilityTable={this.abilityTable} />
-                                                        </ReactToolTip>
-                                                    </React.Fragment>
-                                                )
-                                            })
-                                        })}
-                                    </tbody>
-                                </table> : null 
+                                            return (
+                                                <React.Fragment>
+                                                    <tr data-tip
+                                                        data-for={`${ability.id}-tip`}
+                                                        style={{cursor: "pointer"}}
+                                                        key={`ability-${ability.id}`}>
+                                                            <td>{ability.name}</td>
+                                                            <td>{ability.ap}</td>
+                                                            <td>{ability.element}</td>
+                                                            <td>
+                                                                <button onClick={() => {navigator.clipboard.writeText(ability.id);toast("Copied id to clipboard", {type: "info"});}}>Get Id</button>
+                                                            </td>
+                                                    </tr>
+                                                    <ReactToolTip id={`${ability.id}-tip`} place="right" effect="solid" delayHide={500} delayShow={500} delayUpdate={500}>
+                                                        <AbilityElement 
+                                                            ability={ability} 
+                                                            abilityTable={this.abilityTable} />
+                                                    </ReactToolTip>
+                                                </React.Fragment>
+                                            )
+                                        })
+                                    })}
+                                </tbody>
+                            </table> : null 
+                        }
+                    </div>
+                </div>
+                <div>
+                    <h3>Inventory</h3>
+                    { ["HAND", "OFFHAND", "HEAD", "BODY", "ARMS", "LEGS", "ACCESSORY", "INVENTORY"].map((slot) => {
+                        return Object.keys(user.condensedInventory).map((itemKey) => {
+                            const item = user.condensedInventory[itemKey].item;
+                            const count = user.condensedInventory[itemKey].count;
+                            if (item.slot.toUpperCase() !== slot) {
+                                return;
                             }
-                        </div>
-                    </div>
-                    <div>
-                        <h3>Inventory</h3>
-                        { ["HAND", "OFFHAND", "HEAD", "BODY", "ARMS", "LEGS", "ACCESSORY", "INVENTORY"].map((slot) => {
-                            return Object.keys(user.condensedInventory).map((itemKey) => {
-                                const item = user.condensedInventory[itemKey].item;
-                                const count = user.condensedInventory[itemKey].count;
-                                if (item.slot.toUpperCase() !== slot) {
-                                    return;
-                                }
-                                return (
-                                    <ItemElement 
-                                        item={item} 
-                                        abilityTable={this.abilityTable} 
-                                        count={count} 
-                                        onEquip={(item) => {this.equipItemOnUser(item)}} 
-                                        onSell={(item) => {this.sellItem(item)}} />
-                                )
-                            })}
-                        )}
-                    </div>
+                            return (
+                                <ItemElement 
+                                    item={item} 
+                                    abilityTable={this.abilityTable} 
+                                    count={count} 
+                                    onEquip={(item) => {this.equipItemOnUser(item)}} 
+                                    onSell={(item) => {this.sellItem(item)}} />
+                            )
+                        })}
+                    )}
                 </div>
             </div>
         );
