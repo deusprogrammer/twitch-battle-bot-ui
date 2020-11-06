ad8278866307f64bb8679ecf1a074cc60ae5f8fb
Michael C. Main <deusprogrammer@gmail.com>
2020-11-06 03:01:46 -0600

    Cleaning up UI

--- src/elements/AbilityElement.jsx
+++ src/elements/AbilityElement.jsx
@@ -83,7 +83,7 @@ let AbilityElement = (props) => {
                             return (
                                 <React.Fragment>
                                     <div data-tip data-for={`${ability.name}-trigger-${index}`} className="action-trigger" style={{float: "left", border: "1px solid black", cursor: "pointer", color, backgroundColor}}>{ability.name} ({trigger.chance}%)</div>
-                                    <ReactToolTip id={`${ability.name}-trigger-${index}`}>
+                                    <ReactToolTip id={`${ability.name}-trigger-${index}`} delayHide={2000}>
                                         <AbilityElement ability={ability} abilityTable={abilityTable} />
                                     </ReactToolTip>
                                 </React.Fragment>
