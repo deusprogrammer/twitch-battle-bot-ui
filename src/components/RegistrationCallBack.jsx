6d245f1d476a7351e4e3d511f55d2c5cf09355c4
Michael C. Main <deusprogrammer@gmail.com>
2020-11-12 04:47:09 -0600

    Fixed callback page...I think

--- src/components/RegistrationCallBack.jsx
+++ src/components/RegistrationCallBack.jsx
@@ -4,13 +4,15 @@ import ApiHelper from '../utils/ApiHelper';
 
 export default class RegistrationCallBack extends React.Component {
     state = {
-        error: {}
+        error: {},
+        ready: false
     }
 
     componentDidMount = async () => {
         let queryParam = new URLSearchParams(window.location.search);
         try {
             await ApiHelper.createBot(queryParam.get("code"));
+            this.setState({ready: true});
         } catch (error) {
             this.setState({error});
         }
@@ -26,10 +28,18 @@ export default class RegistrationCallBack extends React.Component {
             )
         }
 
+        if (this.state.ready) {
+            return (
+                <div>
+                    <h1>Thank You</h1>
+                    <p>Your user has been created and your bot has been registered.  You may now use any of the admin console features above to create monsters, items, abilities, and such that belong to your channel.  Please refer to the guide for information about how these ideas work.</p>
+                </div>
+            )
+        }
+
         return (
             <div>
-                <h1>Thank You</h1>
-                <p>Your user has been created and your bot has been registered.  You may now use any of the admin console features above to create monsters, items, abilities, and such that belong to your channel.  Please refer to the guide for information about how these ideas work.</p>
+                <h1>Hold on...we are registering your bot right now.</h1>
             </div>
         )
     }
