5fb3f7add914c2f1540e9fd07bafb6fa6e93a956
Michael C. Main <deusprogrammer@gmail.com>
2020-11-16 19:34:44 -0600

    Fixing headers and removing access token on registration start

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -19,12 +19,12 @@ export default class Bot extends React.Component {
                 <p>Bring the below into your XSplit or OBS presentation layouts to show monsters and battle notifications.  It is recommended to place the encounter panel on either side of the screen, and the notification panel on the top or bottom of the screen.</p>
                 <div style={{display: "table"}}>
                     <div style={{display: "table-row"}}>
-                        <div style={{display: "table-cell"}}>Encounters Panel:</div>
-                        <div style={{display: "table-cell"}}><input type="text" value={`https://deusprogrammer.com/util/battle-panel/encounters?channelId={this.state.channelId}`} style={{width: "700px"}} /></div>
+                        <div style={{display: "table-cell", padding: "10px", fontWeight: "bolder"}}>Encounters Panel:</div>
+                        <div style={{display: "table-cell", padding: "10px"}}><input type="text" value={`https://deusprogrammer.com/util/battle-panel/encounters?channelId={this.state.channelId}`} style={{width: "700px"}} /></div>
                     </div>
                     <div style={{display: "table-row"}}>
-                        <div style={{display: "table-cell"}}>Notifications Panel:</div>
-                        <div style={{display: "table-cell"}}><input type="text" value={`https://deusprogrammer.com/util/battle-panel/notifications?channelId={this.state.channelId}`} style={{width: "700px"}} /></div>
+                        <div style={{display: "table-cell", padding: "10px", fontWeight: "bolder"}}>Notifications Panel:</div>
+                        <div style={{display: "table-cell", padding: "10px"}}><input type="text" value={`https://deusprogrammer.com/util/battle-panel/notifications?channelId={this.state.channelId}`} style={{width: "700px"}} /></div>
                     </div>
                 </div>
                 <h3>Actions</h3>
