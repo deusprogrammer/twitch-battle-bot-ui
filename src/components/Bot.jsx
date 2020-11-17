aea49d286ac9ce172bf3083f411d9b34cfc47f47
Michael C. Main <deusprogrammer@gmail.com>
2020-11-16 19:32:40 -0600

    Fixing headers and removing access token on registration start

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -17,11 +17,15 @@ export default class Bot extends React.Component {
                 <h1>Your Bot</h1>
                 <h3>Panel URLs</h3>
                 <p>Bring the below into your XSplit or OBS presentation layouts to show monsters and battle notifications.  It is recommended to place the encounter panel on either side of the screen, and the notification panel on the top or bottom of the screen.</p>
-                <div>
-                    <span><strong>Encounters Panel:</strong>&nbsp;&nbsp;&nbsp;<input type="text" value="https://deusprogrammer.com/util/battle-panel/encounters?channelId={this.state.channelId}" /></span>
-                </div>
-                <div>
-                    <span><strong>Notification Panel:</strong>&nbsp;<input type="text" value="https://deusprogrammer.com/util/battle-panel/notifications?channelId={this.state.channelId}" /></span>
+                <div style={{display: "table"}}>
+                    <div style={{display: "table-row"}}>
+                        <div style={{display: "table-cell"}}>Encounters Panel:</div>
+                        <div style={{display: "table-cell"}}><input type="text" value={`https://deusprogrammer.com/util/battle-panel/encounters?channelId={this.state.channelId}`} style={{width: "700px"}} /></div>
+                    </div>
+                    <div style={{display: "table-row"}}>
+                        <div style={{display: "table-cell"}}>Notifications Panel:</div>
+                        <div style={{display: "table-cell"}}><input type="text" value={`https://deusprogrammer.com/util/battle-panel/notifications?channelId={this.state.channelId}`} style={{width: "700px"}} /></div>
+                    </div>
                 </div>
                 <h3>Actions</h3>
                 <button>Start</button><button>Stop</button><button>Restart</button>
