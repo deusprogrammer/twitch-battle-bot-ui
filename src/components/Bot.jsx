9af95b71168c1ff6a17afddbe18c45358e248519
Michael C. Main <deusprogrammer@gmail.com>
2020-11-16 19:24:15 -0600

    Fixing headers and removing access token on registration start

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -18,10 +18,10 @@ export default class Bot extends React.Component {
                 <h3>Panel URLs</h3>
                 <p>Bring the below into your XSplit or OBS presentation layouts to show monsters and battle notifications.  It is recommended to place the encounter panel on either side of the screen, and the notification panel on the top or bottom of the screen.</p>
                 <div>
-                    <span><strong>Encounters Panel:</strong> https://deusprogrammer.com/util/battle-panel/encounters?channelId={this.state.channelId}</span>
+                    <span><strong>Encounters Panel:</strong>&nbsp;&nbsp;&nbsp;<input type="text" value="https://deusprogrammer.com/util/battle-panel/encounters?channelId={this.state.channelId}" /></span>
                 </div>
                 <div>
-                    <span><strong>Notification Panel:</strong> https://deusprogrammer.com/util/battle-panel/notifications?channelId={this.state.channelId}</span>
+                    <span><strong>Notification Panel:</strong>&nbsp;<input type="text" value="https://deusprogrammer.com/util/battle-panel/notifications?channelId={this.state.channelId}" /></span>
                 </div>
                 <h3>Actions</h3>
                 <button>Start</button><button>Stop</button><button>Restart</button>
