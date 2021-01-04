14b1e7ffbd41824e480adc8cb760b65508191375
Michael C. Main <deusprogrammer@gmail.com>
2021-01-04 03:30:52 -0600

    Unfucking channel issues

--- src/components/RegistrationRefresh.jsx
+++ src/components/RegistrationRefresh.jsx
@@ -9,6 +9,7 @@ export default class RegistrationRefresh extends React.Component {
     }
 
     componentDidMount = async () => {
+        console.log("MOUNTING");
         document.title = `Auth Refresh`;
         let queryParam = new URLSearchParams(window.location.search);
         let channel = parseInt(window.localStorage.getItem("channel"));
