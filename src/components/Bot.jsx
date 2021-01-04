43a272a9c304e9a2c24e8c8f4db5bf32a1a91407
Michael C. Main <deusprogrammer@gmail.com>
2021-01-04 03:37:57 -0600

    Formatting issues

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -50,9 +50,11 @@ export default class Bot extends React.Component {
                         <div style={{display: "table-cell", padding: "10px"}}>{this.state.botState.running ? "Yes" : "No"}</div>
                     </div>
                 </div>
-                <a href={twitchAuthUrl}>
-                    <button>Refresh Authentication</button>
-                </a>
+                <div style={{textAlign: "center"}}>
+                    <a href={twitchAuthUrl}>
+                        <button>Refresh Authentication</button>
+                    </a>
+                </div>
                 <h3>Panel URLs</h3>
                 <p>Bring the below into your XSplit or OBS presentation layouts to show monsters and battle notifications.  It is recommended to place the encounter panel on either side of the screen, and the notification panel on the top or bottom of the screen.</p>
                 <div style={{display: "table"}}>
