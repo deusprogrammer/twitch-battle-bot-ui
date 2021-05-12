50bcc0d5b44b3f933cf3386bbd74bc9c1fe457c3
Michael C. Main <deusprogrammer@gmail.com>
2021-05-11 23:43:45 -0500

    Added media upload functionality

--- src/components/MediaPoolConfig.jsx
+++ src/components/MediaPoolConfig.jsx
@@ -9,25 +9,74 @@ export default class MediaPoolConfig extends React.Component {
         videoPool: [],
         audioPool: [],
         uploadVideoData: "",
-        uploadAudioData: ""
+        uploadAudioData: "",
+        uploadVideoDataUrl: "",
+        uploadAudioDataUrl: "",
+        uploadFileName: ""
     }
 
     componentDidMount = async () => {
-        let allVideos = await ApiHelper.getAllMedia("mp4");
-        let allAudio = await ApiHelper.getAllMedia("mp3");
-        let {videoPool, audioPool} = ApiHelper.getBotConfig(this.state.channelId);
-        videoPool = videoPool.map((element) => {
-            return await ApiHelper.getMediaMetaData(element);
-        });
-        audioPool = audioPool.map((element) => {
-            return await ApiHelper.getMediaMetaData(element);
-        });
-
-        this.setState({allVideos, allAudio, videoPool, audioPool});
+        try {
+            await this.loadMediaData();
+        } catch (e) {
+            console.error(e);
+        }
     }
 
-    onFileLoaded = (e, target) => {
+    loadMediaData = async () => {
+        let allVideos = await ApiHelper.getAllMedia("mp4");
+            let allAudio = await ApiHelper.getAllMedia("mp3");
+            let {videoPool, audioPool} = ApiHelper.getBotConfig(this.state.channelId);
+            videoPool = videoPool.map((element) => {
+                return await ApiHelper.getMediaMetaData(element);
+            });
+            audioPool = audioPool.map((element) => {
+                return await ApiHelper.getMediaMetaData(element);
+            });
 
+            this.setState({allVideos, allAudio, videoPool, audioPool});
+    }
+
+    onFileLoaded = (e) => {
+        let fr = new FileReader();
+        let file = e.target.files[0];
+
+        const uploadFileName = file.name;
+        const lastDot = name.lastIndexOf('.');
+        const ext = name.substring(lastDot + 1);
+
+        fr.onload = () => {
+            let base64Media = fr.result.substring(fr.result.indexOf(',') + 1);
+
+            if (ext === "mp4") {
+                this.setState({uploadVideoData: base64Media, uploadVideoDataUrl: fr.result, uploadFileName});
+            } else if (ext === "mp3") {
+                this.setState({uploadAudioData: base64Media, uploadAudioDataUrl: fr.result, uploadFileName});
+            }
+        }
+
+        fr.readAsDataURL(file);
+    }
+
+    storeMedia = async (type) => {
+        let mediaData = {}
+
+        if (type === "audio") {
+            mediaType.mimeType = "audio/mp3";
+            mediaType.imagePayload = this.state.uploadAudioData;
+        } else if (type === "video") {
+            mediaType.mimeType = "video/mp4";
+            mediaType.imagePayload = this.state.uploadVideoData;
+        } else {
+            return;
+        }
+
+        try {
+            await ApiHelper.storeMedia(mediaData);
+            this.loadMediaData();
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     render() {
@@ -38,7 +87,7 @@ export default class MediaPoolConfig extends React.Component {
                         <h3>Available Audio</h3>
                         <ul>
                             { this.state.allAudio.map((element) => {
-                                return <li>{element.name}<button>Add</button></li>
+                                return <li>{element.title}<button>Add</button></li>
                             })};
                         </ul>
                     </div>
@@ -46,21 +95,22 @@ export default class MediaPoolConfig extends React.Component {
                         <h3>My Audio</h3>
                         <ul>
                             { this.state.audioPool.map((element) => {
-                                return <li>{element.name}<button>Remove</button></li>
+                                return <li>{element.title}<button>Remove</button></li>
                             })};                        
                         </ul>
                     </div>
                 </div>
                 <div>
-                    <input onChange={(e) => {this.onFileLoaded(e, "audio")}} type="file" />
-                    <button>Store Audio</button>
+                    <audio src={this.state.uploadAudioDataUrl} />
+                    <input onChange={(e) => {this.onFileLoaded(e)}} type="file" />
+                    <button onClick={() => {this.storeMedia("audio")}} disabled={this.state.uploadAudioData ? false : true}>Store Audio</button>
                 </div>
                 <div style={{display: "table"}}>
                     <div style={{display: "table-cell"}}>
                         <h3>Available Video</h3>
                         <ul>
                             { this.state.allVideo.map((element) => {
-                                return <li>{element.name}<button>Add</button></li>
+                                return <li>{element.title}<button>Add</button></li>
                             })};
                         </ul>
                     </div>
@@ -68,14 +118,15 @@ export default class MediaPoolConfig extends React.Component {
                         <h3>My Audio</h3>
                         <ul>
                             { this.state.videoPool.map((element) => {
-                                return <li>{element.name}<button>Remove</button></li>
+                                return <li>{element.title}<button>Remove</button></li>
                             })};                        
                         </ul>
                     </div>
                 </div>
                 <div>
-                    <input onChange={(e) => {this.onFileLoaded(e, "video")}} type="file" />
-                    <button>Store Video</button>
+                    <video src={this.state.uploadVideoDataUrl} />
+                    <input onChange={(e) => {this.onFileLoaded(e)}} type="file" />
+                    <button onClick={() => {this.storeMedia("video")}} disabled={this.state.uploadVideoData ? false : true}>Store Video</button>
                 </div>
             </div>
         )
