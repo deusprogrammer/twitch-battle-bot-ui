3263d166501a3780e42b48dfb4c12824a9d1de3c
Michael Main <deusprogrammer@gmail.com>
2021-06-15 22:04:37 -0500

    Added delete button

--- src/components/MediaPoolConfig.jsx
+++ src/components/MediaPoolConfig.jsx
@@ -94,7 +94,38 @@ export default class MediaPoolConfig extends React.Component {
             } else {
                 return;
             }
-            toast(`Updated video state`, {type: "info"});
+            toast(`Disabled media`, {type: "info"});
+            this.setState({saving: false});
+        } catch(e) {
+            console.error(e);
+            toast("Failed to update media pool!")
+            return;
+        }
+    }
+
+    onDeleteMedia = async (e, type, index) => {
+        let mediaPool = {};
+        if (type === "audio") {
+            mediaPool = [...this.state.audioPool];
+        } else if (type === "video") {
+            mediaPool = [...this.state.videoPool];
+        } else {
+            return;
+        }
+
+        mediaPool.splice(index, 1);
+
+        try {
+            if (type === "audio") {
+                this.setState({audioPool: mediaPool, saving: true});
+                await ApiHelper.updateBotMediaPool(this.state.channelId, "audio", mediaPool);
+            } else if (type === "video") {
+                this.setState({videoPool: mediaPool, saving: true});
+                await ApiHelper.updateBotMediaPool(this.state.channelId, "video", mediaPool);
+            } else {
+                return;
+            }
+            toast(`Deleted media`, {type: "info"});
             this.setState({saving: false});
         } catch(e) {
             console.error(e);
@@ -245,6 +276,12 @@ export default class MediaPoolConfig extends React.Component {
                             { this.state.videoPool.map((element, index) => {
                                 return (<li>
                                             <input type="checkbox" onChange={(e) => {this.onDisableMedia(e, "video", index)}} checked={!element.url.startsWith("*")} disabled={this.state.saving}/>
+                                            <span style={{cursor: "pointer"}} onClick={() => {this.onDeleteMedia()}}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                                </svg>
+                                            </span>
                                             <span className={this.state.videoPreview === element.url  ? "selected" : ""} style={{cursor: "pointer"}} onClick={() => {this.setState({videoPreview: element.url.replace("*", "") })}}>
                                                 <input type="text" value={element.name} onChange={(e) => {this.updateMedia(e, index, "video")}} onBlur={() => {this.saveMediaConfig("video")}} disabled={this.state.saving} />
                                             </span>
