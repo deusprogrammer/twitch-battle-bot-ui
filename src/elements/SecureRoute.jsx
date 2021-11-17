b0b7a8b29c747501048c27de327416ce8cad4633
Michael Main <deusprogrammer@gmail.com>
2021-11-17 11:11:50 -0600

    Fixing my secure route

--- src/elements/SecureRoute.jsx
+++ src/elements/SecureRoute.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router';
 
 export default (props) => {
+    useEffect(() => {
+        window.onblur = function() {window.onfocus = function () {window.location.reload(true)}};
+    }, [])
+
     if (!props.isAuthenticated) {
         delete props.component;
         return <Route {...props} render={props => <div>You must be authenticated to use this page.  Please login.</div>} />
